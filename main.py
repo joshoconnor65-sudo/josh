b5ab1f73bb0e099fbaf20f813b25673e3da45714
Ivan <vankata294n4@gmail.com>
2023-03-10 01:08:36 +0200

    Added script that bypass minecraft protection against mouse movement scripts

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import time
 import pytesseract
 from PIL import Image
+import pydirectinput
 coordOptions = {
     0: 'x',
     1: 'y',
@@ -12,25 +13,26 @@ coordOptions = {
 }
 
 time.sleep(2)
-while True:
-    pic = pyautogui.screenshot(region=(320, 210, 290, 50))
-    # pic.save('ok.png')
-    coords = pytesseract.image_to_string(pic)
-    # coords = coords.split(' ')
-    print(coords.split(','), 'coords')
-    coords = coords.split(',')
-    i = 0
-    for coord in coords:
-        coord = coord.replace(')', ' ')
-        coord = coord.replace('(', ' ') 
-        coord = coord.replace('/', ' ') 
-        coord = coord.replace('|', ' ') 
-        try:
-            coord = int(coord)
+pydirectinput.click(240, 240)
+# while True:
+#     pic = pyautogui.screenshot(region=(320, 210, 290, 50))
+#     # pic.save('ok.png')
+#     coords = pytesseract.image_to_string(pic)
+#     # coords = coords.split(' ')
+#     print(coords.split(','), 'coords')
+#     coords = coords.split(',')
+#     i = 0
+#     for coord in coords:
+#         coord = coord.replace(')', ' ')
+#         coord = coord.replace('(', ' ') 
+#         coord = coord.replace('/', ' ') 
+#         coord = coord.replace('|', ' ') 
+#         try:
+#             coord = int(coord)
 
-            print(f"{coordOptions[i]}={coord}")
-            i += 1
-        except:
-            print('error')
+#             print(f"{coordOptions[i]}={coord}")
+#             i += 1
+#         except:
+#             print('error')
         
-    time.sleep(0.1)
+#     time.sleep(0.1)
---
+++ moveMouse.ahk
@@ -0,0 +1,2 @@
+Sleep 3000
+DllCall("mouse_event", uint, 1, int, -300, int, 200)
